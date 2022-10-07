74dc5d4e1c4c443029bcf0c4dc489a680b89980f
Marco Gramatica <gramaticamarco@gmail.com>
2022-10-07 22:43:23 +0100

    Amended plots and stuff. Still missing plot for 2015 many species for NO REASON

--- app.R
+++ app.R
@@ -8,6 +8,10 @@ load(gsub("Data/Envir/", "", interest_id_label))
 ## Load regression results
 load(gsub("Data/", "", reg_label))
 
+# load(npms_label)
+# load(interest_id_label)
+# load(reg_label)
+
 library(dplyr)
 library(lubridate)
 library(tidyr)
@@ -43,7 +47,8 @@ ui <- fluidPage(
         selectInput(
           "spec", "Species", sel_kew_id
         ),
-        checkboxInput("pool_y", "Pool years", value = T)
+        checkboxInput("pool_y", "Pool years", value = F),
+        checkboxInput("n_trig", "Number of species", value = F)
       )
     ),
     column(
@@ -52,23 +57,39 @@ ui <- fluidPage(
         plotOutput("n_plot", height = 320, width = 500),
       ),
       conditionalPanel(
-        condition = "input.pool_y == 0",
+        condition = "input.pool_y == 0 && input.n_trig == 0",
         uiOutput("plots_ind")
       ),
       conditionalPanel(
-        condition = "input.pool_y == 1",
+        condition = "input.pool_y == 1 && input.n_trig == 0",
         plotOutput("plots_pooled")
+      ),
+      conditionalPanel(
+        condition = "input.pool_y == 0 && input.n_trig == 1",
+        uiOutput("plots_ind_n")
+      ),
+      conditionalPanel(
+        condition = "input.pool_y == 1 && input.n_trig == 1",
+        plotOutput("plots_pooled_n")
       )
     ),
     column(
       5, "",
       conditionalPanel(
-        condition = "input.pool_y == 0",
+        condition = "input.pool_y == 0 && input.n_trig == 0",
         uiOutput("tabs_ind")
       ),
       conditionalPanel(
-        condition = "input.pool_y == 1",
+        condition = "input.pool_y == 1 && input.n_trig == 0",
         htmlOutput("tabs_pooled")
+      ),
+      conditionalPanel(
+        condition = "input.pool_y == 0 && input.n_trig == 1",
+        uiOutput("tabs_ind_n")
+      ),
+      conditionalPanel(
+        condition = "input.pool_y == 1 && input.n_trig == 1",
+        htmlOutput("tabs_pooled_n")
       )
     )
   )
@@ -118,6 +139,7 @@ server <- function(input, output) {
   
   # OUTPUT - PLOTS ----
   ## POOLED
+  ### ABUNDANCE
   output$plots_pooled <- renderPlot({
     ## Compute regression
     reg <- f_reg(kew_local = kew_id(), pool = T)
@@ -138,7 +160,29 @@ server <- function(input, output) {
         axis.text.y = element_text(size = 18, face = "bold")
       ) + ggtitle("Pooled years")
   })
+  ### NUMBER OF SPECIES
+  output$plots_pooled_n <- renderPlot({
+    ## Compute regression
+    reg <- f_reg(kew_local = kew_id(), pool = T, no = T)
+    ## Bind results
+    reg_predict <- cbind(
+      pooled_plant |> select(n_native, kew_id()) |> filter(get(kew_id()) != 0),
+      predict(reg, interval = 'confidence')
+    )
+    
+    ggplot(reg_predict, aes(reg_predict[, paste(kew_id())], n_native)) +
+      geom_point() +
+      geom_line(aes(reg_predict[, paste(kew_id())], fit)) +
+      geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.3) + theme_bw() +
+      xlab(paste(input$spec)) + ylab("Number of native species") +
+      theme(
+        title = element_text(size = 20, face = "bold"),
+        axis.text.x = element_text(size = 18, face = "bold"),
+        axis.text.y = element_text(size = 18, face = "bold")
+      ) + ggtitle("Pooled years")
+  })
   ## INDIVIDUAL
+  ### ABUNDANCE
   output$plots_ind <- renderUI({
     plot_output_list <- lapply(2015:2021, function(i) {
       plotname <- paste("plot", i, sep="")
@@ -149,7 +193,6 @@ server <- function(input, output) {
     # to display properly.
     do.call(tagList, plot_output_list)
   })
-  ## Generate plots
   for(i in 2015:2021){
     # Need local so that each item gets its own number. Without it, the value
     # of i in the renderPlot() will be the same across all instances, because
@@ -192,18 +235,90 @@ server <- function(input, output) {
       })
     })
   }
+  ### NUMBER OF SPECIES
+  output$plots_ind_n <- renderUI({
+    plot_output_list <- lapply(2015:2021, function(i) {
+      plotname <- paste("plot_n", i, sep="")
+      plotOutput(plotname, height = 320, width = 500)
+    })
+    
+    # Convert the list to a tagList - this is necessary for the list of items
+    # to display properly.
+    do.call(tagList, plot_output_list)
+  })
+  for(i in 2015:2021){
+    # Need local so that each item gets its own number. Without it, the value
+    # of i in the renderPlot() will be the same across all instances, because
+    # of when the expression is evaluated.
+    local({
+      my_i <- i
+      
+      # Function to actually render a plot
+      plotname <- paste("plot_n", my_i, sep = "")
+      output[[plotname]] <- renderPlot({
+        # Calculate regression
+        reg <- f_reg(year = my_i, kew_local = kew_id(), no = T)
+        
+        # Bind results
+        reg_predict <- cbind(
+          pivot_reac(year = my_i, kew_local = kew_id(), no = T) |> 
+            na.omit(), predict(reg, interval = 'confidence')
+        )
+        
+        # Produce plot
+        ggplot(reg_predict, aes(reg_predict[, paste(kew_id())], n_native)) +
+          geom_point() + 
+          geom_line(aes(reg_predict[, paste(kew_id())], fit)) +
+          geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.3) + theme_bw() +
+          xlab(paste(input$spec)) + ylab("Number of native species") +
+          ggtitle(paste(my_i)) +
+          theme(
+            title = element_text(size = 20, face = "bold"),
+            axis.text.x = element_text(size = 18, face = "bold"),
+            axis.text.y = element_text(size = 18, face = "bold")
+          )
+      })
+      
+      # Tables 
+      tablename <- paste("table_n", my_i, sep = "")
+      output[[tablename]] <- reactive({
+        html_reg <- f_reg(year = my_i, kew_local = kew_id(), no = T) |>
+          sjPlot::tab_model(dv.labels = paste(my_i))
+        HTML(html_reg$page.content, html_reg$page.style)
+      })
+    })
+  }
   
   # TABLES ----
   ## POOLED TABLE
+  ### ABUNDANCE
   output$tabs_pooled <- reactive({
     html_reg <- f_reg(kew_local = kew_id(), pool = T) |>
       sjPlot::tab_model(dv.labels = "Pooled")
     HTML(html_reg$page.content, html_reg$page.style)
   })
+  output$tabs_pooled_n <- reactive({
+    html_reg <- f_reg(kew_local = kew_id(), pool = T, no = T) |>
+      sjPlot::tab_model(dv.labels = "Pooled")
+    HTML(html_reg$page.content, html_reg$page.style)
+  })
+  ### NUMBER OF SPECIES
   ## INDIVIDUAL
+  ### ABUNDANCE
   output$tabs_ind <- renderUI({
     table_output_list <- lapply(2015:2021, function(i) {
       tablename <- paste("table", i, sep="")
+      htmlOutput(tablename)
+    })
+    
+    # Convert the list to a tagList - this is necessary for the list of items
+    # to display properly.
+    do.call(tagList, table_output_list)
+  })
+  ### NUMBER OF SPECIES
+  output$tabs_ind_n <- renderUI({
+    table_output_list <- lapply(2015:2021, function(i) {
+      tablename <- paste("table_n", i, sep="")
       htmlOutput(tablename)
     })
     
@@ -219,44 +334,56 @@ server <- function(input, output) {
       as.character()
   })
   ## Data
-  pivot_reac <- function(year, kew_local, pool = F){
-    # Pooled year trigger
-    if(pool == T){
-      # Id's for adding up native abundance
-      id_native <- d_plant |> 
-        filter(StaceIV_nativity == "N") |> pull(kew_id) |> unique()
-      ## Filter dataset
-      filtered_data <- d_plant |> filter(is.na(StaceIV_nativity) == F)
-    } else{
-      # Id's for adding up native abundance
-      id_native <- d_plant |> 
-        filter(StaceIV_nativity == "N", year(date) == year) |>
-        pull(kew_id) |> unique()
-      ## Filter dataset
-      filtered_data <- d_plant |> 
-        filter(is.na(StaceIV_nativity) == F, year(date) == year)
-    }
-    
-    # Pivot data
-    filtered_data |> distinct(kew_id, location_id, .keep_all = T) |>
+  pivot_reac <- function(year, kew_local, no = F){
+    # Id's for adding up native abundance
+    id_native <- d_plant |> 
+      filter(StaceIV_nativity == "N", year(date) == year) |>
+      pull(kew_id) |> unique()
+    ## Filter dataset
+    filtered_data <- d_plant |> 
+      filter(is.na(StaceIV_nativity) == F, year(date) == year) |>
+      distinct(kew_id, location_id, .keep_all = T) |>
       select(kew_id, midPoint, location_id) |>
       pivot_wider(
         names_from = kew_id, values_from = midPoint, values_fill = NA
       ) |> mutate(
-        ab_native = rowSums(across(.cols = contains(id_native)), na.rm = T)
-      ) |> select(ab_native, kew_local) |> as.data.frame()
+        ab_native = rowSums(across(.cols = contains(id_native)), na.rm = T),
+        n_native = rowSums(is.na(across(.cols = contains(id_native))) == F)
+      )
+    
+    # Pivot data
+    if(no == T){
+      filtered_data |> select(n_native, kew_local) |> as.data.frame()
+    } else{
+      filtered_data |> select(ab_native, kew_local) |> as.data.frame()
+    }
   }
   ## Regression
-  f_reg <- function(year, kew_local, pool = F){
-    # browser()s
+  f_reg <- function(year, kew_local, pool = F, no = F){
+    # browser()
     if(pool == T){
       ## Pooled years
-      lm(data = pooled_plant |> select(ab_native, kew_local) |> 
-           filter(get(kew_local) != 0) |> as.data.frame(), 
-         ab_native ~ .)
+      if(no == T){
+        lm(data = pooled_plant |> select(n_native, kew_local) |> 
+             filter(get(kew_local) != 0) |> as.data.frame(), 
+           n_native ~ .)
+      } else{
+        lm(data = pooled_plant |> select(ab_native, kew_local) |> 
+             filter(get(kew_local) != 0) |> as.data.frame(), 
+           ab_native ~ .)
+      }
     } else{
       ## Individual year
-      lm(data = pivot_reac(year = year, kew_local = kew_local), ab_native ~ .)
+      if(no == T){
+        lm(
+          data = pivot_reac(year = year, kew_local = kew_local, no = T), 
+          n_native ~ .
+        )
+      } else{
+        lm(
+          data = pivot_reac(year = year, kew_local = kew_local), ab_native ~ .
+        )
+      }
     }
   }
 }
